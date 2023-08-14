e70d57112f9dafa76193e2eac64ee59ad8d8ea25
youngniiiiii <hanuje_@naver.com>
2023-08-14 21:51:41 +0900

    propTypes , 버튼,인풋추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import ButtonClick from '@/Component/ButtonClick'
+import React, { useState } from 'react'
+import { ButtonClick } from '@/Component/ButtonClick'
 import '@/Component/ButtonClick.css'
+import { Button, Checkbox, SubmitButton, CheckboxAll } from './Component/ButtonClick'
 
 function App() {
+  //[상태,set상태]
+  const [isChecked, setIsChecked] = useState(false)
+  const [isToggled, setIsToggled] = useState(false)
+  const [isSecondToggled, setIsSecondToggled] = useState(false)
+
+  //이벤트 핸들링
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked)
+  }
+
+  const handleToggleButton = () => {
+    setIsToggled(!isToggled)
+  }
+  const handleSecondToggleButton = () => {
+    setIsSecondToggled(!isSecondToggled)
+  }
+  const handleSubmit = () => {
+    alert(`Checkbox: ${isChecked}, Toggle: ${isToggled}`)
+  }
+
   return (
-    <>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h1>React 1주차 과제</h1>
-      <ButtonClick buttonClick="deSelect" /> <ButtonClick buttonClick="Select" />
-    </>
+      <div style={{ padding: '20px' }}>
+        <ButtonClick buttonClick="deSelect" />
+        <CheckboxAll buttonClick="Select" />
+      </div>
+      <Checkbox isChecked={isChecked} onChange={handleCheckboxChange} />
+      <div style={{ padding: '20px' }}>
+        <Button text={isToggled ? '토글 OFF' : '토글 ON'} onClick={handleToggleButton} />
+        <Button text={isSecondToggled ? '토글 OFF' : '토글 ON'} onClick={handleSecondToggleButton} />
+      </div>
+      <SubmitButton onClick={handleSubmit} />
+    </div>
   )
 }
 
--- src/Component/ButtonClick.jsx
+++ src/Component/ButtonClick.jsx
@@ -1,10 +1,63 @@
-function ButtonClick({ buttonClick }) {
-  console.log(buttonClick)
+import PropTypes from 'prop-types'
+
+export const CheckboxAll = (isChecked) => {
   return (
     <>
-      <button type="button" className={buttonClick === 'deSelect' ? 'checked' : 'unChecked'} />
+      <span>체크박스</span>
+      <button type="button" className={isChecked === 'deSelect' ? 'checked' : 'unChecked'} />
     </>
   )
 }
 
-export default ButtonClick
+CheckboxAll.propTypes = {
+  isChecked: PropTypes.bool,
+}
+
+export function ButtonClick({ isChecked }) {
+  console.log(isChecked)
+  return (
+    <>
+      <span>버튼</span>
+      <button type="button" className={isChecked === 'deSelect' ? 'checked' : 'unChecked'} />
+    </>
+  )
+}
+ButtonClick.propTypes = {
+  isChecked: PropTypes.string,
+}
+
+export function Button({ text, onClick }) {
+  return <button onClick={onClick}>{text}</button>
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+export function Checkbox({ isChecked, onChange }) {
+  return (
+    <label>
+      <input type="checkbox" checked={isChecked} onChange={onChange} />
+      체크박스
+    </label>
+  )
+}
+
+Checkbox.propTypes = {
+  isChecked: PropTypes.bool,
+  onChange: PropTypes.func,
+}
+
+// export default ButtonClick
+export function SubmitButton({ onClick }) {
+  return (
+    <button type="submit" onClick={onClick}>
+      제출
+    </button>
+  )
+}
+
+SubmitButton.propTypes = {
+  onClick: PropTypes.func,
+}
